be8f0477bcbca480f4374a8e963f5690eec910db
Wolfgang Bumiller <w.bumiller@proxmox.com>
2015-10-30 10:31:01 +0100

    fix CLONE constant declaration

--- src/PVE/Tools.pm
+++ src/PVE/Tools.pm
@@ -67,12 +67,12 @@ our $IPV6RE = "(?:" .
 
 our $IPRE = "(?:$IPV4RE|$IPV6RE)";
 
-use constant (CLONE_NEWNS   => 0x00020000,
+use constant {CLONE_NEWNS   => 0x00020000,
               CLONE_NEWUTS  => 0x04000000,
               CLONE_NEWIPC  => 0x08000000,
               CLONE_NEWUSER => 0x10000000,
               CLONE_NEWPID  => 0x20000000,
-              CLONE_NEWNET  => 0x40000000);
+              CLONE_NEWNET  => 0x40000000};
 
 sub run_with_timeout {
     my ($timeout, $code, @param) = @_;
