proxmox/pve-common

commit 24197a9f6c698985b7255fbf7792b0b6bd8188b5

use Encode::Locale to encode parameters read from console(getopt)

src/PVE/JSONSchema.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Storable; # for dclone
 use Getopt::Long;
+use Encode::Locale;
+use Encode;
 use Devel::Cycle -quiet; # todo: remove?
 use PVE::Tools qw(split_list $IPV6RE $IPV4RE);
 use PVE::Exception qw(raise);
@@ -1349,7 +1351,9 @@ sub get_options {
 	}
     }
 
-    $opts = PVE::Tools::decode_utf8_parameters($opts);
+    foreach my $p (keys %$opts) {
+	$opts->{$p} = decode('locale', $opts->{$p});
+    }
 
     foreach my $p (keys %$opts) {
 	if (my $pd = $schema->{properties}->{$p}) {

commit c99025689644aae5cf6c427de4cd704dc9d352c7

get_options: handle array and scalar refs on decoding
get_options is for parsing CLI options, here we decode after using
Getopt as we are not sure how well it handles already decoded data.
But as Gettopt can produces references for the parsed data we must
handle them explictily.
So check if we have a ARRAY or SCALAR reference and decode them
respectively.
All other reference types should not get returned from Getopt so
error out on them.

This bug was seen when viewing backup jobs, as we save the job as a
comand entry in /etc/pve/vzdump.cron and parse it then with this
function on reading.
Besides the use there we use it in the RESTHandler Packages
cli_handler sub method, so some CLI tools could be possibly affected
by this.

Fixes: 24197a9f6c698985b7255fbf7792b0b6bd8188b5
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/JSONSchema.pm
@@ -1351,8 +1351,21 @@ sub get_options {
 	}
     }
 
+    # decode after Getopt as we are not sure how well it handles unicode
     foreach my $p (keys %$opts) {
-	$opts->{$p} = decode('locale', $opts->{$p});
+	if (!ref($opts->{$p})) {
+	    $opts->{$p} = decode('locale', $opts->{$p});
+	} elsif (ref($opts->{$p}) eq 'ARRAY') {
+	    my $tmp = [];
+	    foreach my $v (@{$opts->{$p}}) {
+		push @$tmp, decode('locale', $v);
+	    }
+	    $opts->{$p} = $tmp;
+	} elsif (ref($opts->{$p}) eq 'SCALAR') {
+	    $opts->{$p} = decode('locale', $$opts->{$p});
+	} else {
+	    raise("decoding options failed, unknown reference\n", code => HTTP_BAD_REQUEST);
+	}
     }
 
     foreach my $p (keys %$opts) {