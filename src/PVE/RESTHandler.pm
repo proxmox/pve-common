proxmox/pve-common

commit 25d9bda94127b1a91181cd80bb9948093dea389b

schema_get_type_text: render generic types as <type>

src/PVE/RESTHandler.pm
@@ -443,7 +443,7 @@ my $get_property_description = sub {
     if ($fileparams && $type eq 'string') {
 	foreach my $elem (@$fileparams) {
 	    if ($name eq $elem) {
-		$type = 'filepath';
+		$type = '<filepath>';
 		last;
 	    }
 	}
@@ -466,7 +466,7 @@ my $get_property_description = sub {
 	$res .= "`$type` " if $type;
 
 	if (defined(my $dv = $phash->{default})) {
-	    $res .= "(default=`$dv`)";
+	    $res .= "('default =' `$dv`)";
 	}
 
 	if ($style eq 'config-sub') {

src/PVE/JSONSchema.pm
@@ -1683,18 +1683,18 @@ sub schema_get_type_text {
     } elsif ($phash->{format_description}) {
 	return "<$phash->{format_description}>";
     } elsif ($phash->{enum}) {
-	return "(" . join(' | ', sort @{$phash->{enum}}) . ")";
+	return "<" . join(' | ', sort @{$phash->{enum}}) . ">";
     } elsif ($phash->{pattern}) {
 	return $phash->{pattern};
     } elsif ($type eq 'integer' || $type eq 'number') {
 	# NOTE: always access values as number (avoid converion to string)
 	if (defined($phash->{minimum}) && defined($phash->{maximum})) {
-	    return "$type (" . ($phash->{minimum} + 0) . " - " .
+	    return "<$type> (" . ($phash->{minimum} + 0) . " - " .
 		($phash->{maximum} + 0) . ")";
 	} elsif (defined($phash->{minimum})) {
-	    return "$type (" . ($phash->{minimum} + 0) . " - N)";
+	    return "<$type> (" . ($phash->{minimum} + 0) . " - N)";
 	} elsif (defined($phash->{maximum})) {
-	    return "$type (-N - " . ($phash->{maximum} + 0) . ")";
+	    return "<$type> (-N - " . ($phash->{maximum} + 0) . ")";
 	}
     } elsif ($type eq 'string') {
 	if (my $format = $phash->{format}) {
@@ -1705,7 +1705,7 @@ sub schema_get_type_text {
 	}
     }
 
-    return $type;
+    return "<$type>";
 }
 
 1;

commit fbb0ec640ac1c971da55491c68d32b82c984bc32

api: fix rendering of filepath/string parameters
25d9bda94127b1a91181cd80bb9948093dea389b broke this check,
but it is a better idea to check against the actual type
rather then the rendered type text anyway.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

src/PVE/RESTHandler.pm
@@ -415,7 +415,7 @@ my $get_property_description = sub {
 	$type = '';
     }
 
-    if ($fileparams && $type eq 'string') {
+    if ($fileparams && $phash->{type} eq 'string') {
 	foreach my $elem (@$fileparams) {
 	    if ($name eq $elem) {
 		$type = '<filepath>';