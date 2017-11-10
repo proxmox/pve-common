proxmox/pve-common

commit 0da5a3e43b1659431a6e0fa4e38583741d27dbbc

daemon: refactor and cleanup
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/Daemon.pm
@@ -146,11 +146,7 @@ my $start_workers = sub {
 
     return if $self->{terminate};
 
-    my $count = 0;
-    foreach my $cpid (keys %{$self->{workers}}) {
-	$count++;
-    }
-
+    my $count = scalar keys %{$self->{workers}};
     my $need = $self->{max_workers} - $count;
 
     return if $need <= 0;
@@ -289,11 +285,7 @@ sub setup {
 
     if ($restart && $self->{max_workers}) {
 	if (my $wpids = $ENV{PVE_DAEMON_WORKER_PIDS}) {
-	    foreach my $pid (split(':', $wpids)) {
-		if ($pid =~ m/^(\d+)$/) {
-		    $self->{old_workers}->{$1} = 1;
-		}
-	    }
+	    $self->{old_workers}->{$_} = 1 foreach (split(':', $wpids));
 	}
     }
 
@@ -503,8 +495,7 @@ sub restart_daemon {
     }
 
     if ($self->{max_workers}) {
-	my @workers = keys %{$self->{workers}};
-	push @workers, keys %{$self->{old_workers}};
+	my @workers = keys %{$self->{workers}}, keys %{$self->{old_workers}};
 	$ENV{PVE_DAEMON_WORKER_PIDS} = join(':', @workers);
     }
 

commit 563661dd943e969e6df2b97e84043829032794fd

daemon: add missing parenthesis around list
Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
Fixes: 0da5a3e43b16 ("daemon: refactor and cleanup")

src/PVE/Daemon.pm
@@ -490,7 +490,7 @@ sub restart_daemon {
     }
 
     if ($self->{max_workers}) {
-	my @workers = keys %{$self->{workers}}, keys %{$self->{old_workers}};
+	my @workers = (keys %{$self->{workers}}, keys %{$self->{old_workers}});
 	$ENV{PVE_DAEMON_WORKER_PIDS} = join(':', @workers);
     }
 