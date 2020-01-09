c8ff0bdf3dd8606400cf2999c770408f1ef02d84
Alexandre Derumier <aderumier@odiso.com>
2020-01-09 14:11:01 +0100

    INotify: allow vlan tagged bridge interface on non-vlanaware bridge
    Seem that it's working too with non-vlanaware bridge
    
    Signed-off-by: Alexandre Derumier <aderumier@odiso.com>

--- src/PVE/INotify.pm
+++ src/PVE/INotify.pm
@@ -1474,9 +1474,7 @@ sub __write_etc_network_interfaces {
 	    die "vlan '$iface' - unable to find parent '$p'\n"
 		if !$n;
 
-	    if ($n->{type} eq 'bridge' && !$n->{bridge_vlan_aware}) {
-		die "vlan '$iface' - bridge vlan aware is not enabled on parent '$p'\n";
-	    } elsif ($n->{type} ne 'eth' && $n->{type} ne 'bridge' && $n->{type} ne 'bond' && $n->{type} ne 'vlan') {
+	    if ($n->{type} ne 'eth' && $n->{type} ne 'bridge' && $n->{type} ne 'bond' && $n->{type} ne 'vlan') {
 		die "vlan '$iface' - wrong interface type on parent '$p' " .
 		    "('$n->{type}' != 'eth|bond|bridge|vlan' )\n";
 	    }
