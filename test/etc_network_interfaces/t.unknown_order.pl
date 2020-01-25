f82c73738d63958847b50736914249099732cc13
Alexandre Derumier <aderumier@odiso.com>
2020-01-25 10:27:04 +0100

    tests: unknown interfaces order: rename vlan interfaces to unknown
    vlan(X) interfaces are not unknown anymore
    use generic name instead
    
    Signed-off-by: Alexandre Derumier <aderumier@odiso.com>

--- test/etc_network_interfaces/t.unknown_order.pl
+++ test/etc_network_interfaces/t.unknown_order.pl
@@ -39,20 +39,17 @@ iface bond2 inet manual
 	bond-mode balance-alb
 # Private networking
 
-iface vlan3 inet static
+iface unknown3 inet static
 	address  0.0.0.0
 	netmask  0.0.0.0
-	vlan-raw-device bond2
 
-iface vlan4 inet static
+iface unknown4 inet static
 	address  0.0.0.0
 	netmask  0.0.0.0
-	vlan-raw-device bond2
 
-iface vlan5 inet static
+iface unknown5 inet static
 	address  0.0.0.0
 	netmask  0.0.0.0
-	vlan-raw-device bond2
 
 auto vmbr0
 iface vmbr0 inet static
@@ -63,34 +60,33 @@ iface vmbr0 inet static
 	bridge-stp off
 	bridge-fd 0
 
-auto vlan6
-iface vlan6 inet static
+auto unknown6
+iface unknown6 inet static
 	address  10.10.11.13
 	netmask  255.255.255.0
-	vlan_raw_device bond0
 	network 10.10.11.0
 	pre-up ifconfig bond0 up
 
 auto vmbr3
 iface vmbr3 inet manual
-	bridge-ports vlan3
+	bridge-ports unknown3
 	bridge-stp off
 	bridge-fd 0
-	pre-up ifup vlan3
+	pre-up ifup unknown3
 
 auto vmbr4
 iface vmbr4 inet manual
-	bridge-ports vlan4
+	bridge-ports unknown4
 	bridge-stp off
 	bridge-fd 0
-	pre-up ifup vlan4
+	pre-up ifup unknown4
 
 auto vmbr5
 iface vmbr5 inet manual
-	bridge-ports vlan5
+	bridge-ports unknown5
 	bridge-stp off
 	bridge-fd 0
-	pre-up ifup vlan5
+	pre-up ifup unknown5
 
 IFACES
 }
