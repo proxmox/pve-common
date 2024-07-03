proxmox/pve-common

commit b792e8df81d70cc8fc4bc7d0655313d4a7f40c3d

tools: Add mknod syscall
Signed-off-by: Filip Schauer <f.schauer@proxmox.com>

src/PVE/Tools.pm
@@ -1720,6 +1720,11 @@ sub mkdirat($$$) {
     return syscall(PVE::Syscall::mkdirat, int($dirfd), $name, int($mode)) == 0;
 }
 
+sub mknod($$$) {
+    my ($filename, $mode, $dev) = @_;
+    return syscall(PVE::Syscall::SYS_mknod, $filename, int($mode), int($dev)) == 0;
+}
+
 sub fchownat($$$$$) {
     my ($dirfd, $pathname, $owner, $group, $flags) = @_;
     return syscall(

src/PVE/Syscall.pm
@@ -16,6 +16,7 @@ BEGIN {
 	openat => &SYS_openat,
 	close => &SYS_close,
 	mkdirat => &SYS_mkdirat,
+	mknod => &SYS_mknod,
 	faccessat => &SYS_faccessat,
 	setresuid => &SYS_setresuid,
 	fchownat => &SYS_fchownat,

commit 6dc7a73bd50efc72e8260dcda2cf2ec4f158401d

tools: fix syscall mknod()
b792e8df81 introduced a bug that can cause this:

Undefined subroutine &PVE::Syscall::SYS_mknod called at /usr/share/perl5/PVE/Syscall.pm line 11

It should be mknod, not SYS_mknod. This caused other pve perl lib failing
to build. I couldn't reproduce this on amd64 build, but I could reproduce this
on arm64 build; however this didn't seem to fix the issue, unless I revert
b792e8df81.

cf: b792e8df81d70cc8fc4bc7d0655313d4a7f40c3d
Signed-off-by: Jing Luo <jing@jing.rocks>

src/PVE/Tools.pm
@@ -1753,7 +1753,7 @@ sub mkdirat($$$) {
 
 sub mknod($$$) {
     my ($filename, $mode, $dev) = @_;
-    return syscall(PVE::Syscall::SYS_mknod, $filename, int($mode), int($dev)) == 0;
+    return syscall(PVE::Syscall::mknod, $filename, int($mode), int($dev)) == 0;
 }
 
 sub fchownat($$$$$) {