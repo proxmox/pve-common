proxmox/pve-common

commit 4b534406dafc2c86050f80b6a1244125bd81e913

create dedicated PVE::File module
To have common file helpers available without requiring the PVE::Tools
"monster" module, which pulls in half of the available perl modules
directly or indirectly.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/File.pm
@@ -1,0 +1,225 @@
+package PVE::File;
+
+use v5.36;
+
+use IO::File qw(O_CREAT O_DIRECTORY O_EXCL O_RDWR O_WRONLY);
+use IO::Dir ();
+use POSIX qw(EEXIST EOPNOTSUPP);
+
+use base 'Exporter';
+
+our @EXPORT_OK = qw(
+    file_set_contents
+    file_get_contents
+    file_read_first_line
+    dir_glob_regex
+    dir_glob_foreach
+    file_copy
+    O_PATH
+    O_TMPFILE
+    AT_EMPTY_PATH
+    AT_FDCWD
+);
+
+use constant {
+    O_PATH => 0x00200000,
+    O_CLOEXEC => 0x00080000,
+    O_TMPFILE => 0x00400000 | O_DIRECTORY,
+};
+
+use constant {
+    AT_EMPTY_PATH => 0x1000,
+    AT_FDCWD => -100,
+};
+
+# from <linux/fs.h>
+use constant {
+    RENAME_NOREPLACE => (1 << 0),
+    RENAME_EXCHANGE => (1 << 1),
+    RENAME_WHITEOUT => (1 << 2),
+};
+
+sub file_set_contents {
+    my ($filename, $data, $perm, $force_utf8) = @_;
+
+    $perm = 0644 if !defined($perm);
+
+    my $tmpname = "$filename.tmp.$$";
+
+    eval {
+        my ($fh, $tries) = (undef, 0);
+        while (!$fh && $tries++ < 3) {
+            $fh = IO::File->new($tmpname, O_WRONLY | O_CREAT | O_EXCL, $perm);
+            if (!$fh && $! == EEXIST) {
+                unlink($tmpname) or die "unable to delete old temp file: $!\n";
+            }
+        }
+        die "unable to open file '$tmpname' - $!\n" if !$fh;
+
+        if ($force_utf8) {
+            $data = encode("utf8", $data);
+        } else {
+            # Encode wide characters with print before passing them to syswrite
+            my $unencoded_data = $data;
+            # Preload PerlIO::scalar at compile time to prevent runtime loading issues when
+            # file_set_contents is called with PVE::LXC::Setup::protected_call. Normally,
+            # PerlIO::scalar is loaded implicitly during the execution of
+            # `open(my $data_fh, '>', \$data)`. However, this fails if it is executed within a
+            # chroot environment where the necessary PerlIO.pm module file is inaccessible.
+            # Preloading the module ensures it is available regardless of the execution context.
+            use PerlIO::scalar;
+            open(my $data_fh, '>', \$data) or die "failed to open in-memory variable - $!\n";
+            print $data_fh $unencoded_data;
+            close($data_fh);
+        }
+
+        my $offset = 0;
+        my $len = length($data);
+
+        while ($offset < $len) {
+            my $written_bytes = syswrite($fh, $data, $len - $offset, $offset)
+                or die "unable to write '$tmpname' - $!\n";
+            $offset += $written_bytes;
+        }
+
+        close $fh or die "closing file '$tmpname' failed - $!\n";
+    };
+    my $err = $@;
+
+    if ($err) {
+        unlink $tmpname;
+        die $err;
+    }
+
+    if (!rename($tmpname, $filename)) {
+        my $msg = "close (rename) atomic file '$filename' failed: $!\n";
+        unlink $tmpname;
+        die $msg;
+    }
+}
+
+sub file_get_contents($filename, $max) {
+    my $fh = IO::File->new($filename, "r") || die "can't open '$filename' - $!\n";
+
+    my $content = safe_read_from($fh, $max, 0, $filename);
+
+    close $fh;
+
+    return $content;
+}
+
+sub file_copy($filename, $dst, $max, $perm) {
+    file_set_contents($dst, file_get_contents($filename, $max), $perm);
+}
+
+sub file_read_first_line($filename) {
+    my $fh = IO::File->new($filename, "r");
+    if (!$fh) {
+        return undef if $! == POSIX::ENOENT;
+        die "file '$filename' exists but open for reading failed - $!\n";
+    }
+    my $res = <$fh>;
+    chomp $res if $res;
+    $fh->close;
+    return $res;
+}
+
+sub safe_read_from($fh, $max, $oneline, $filename) {
+    # pmxcfs file size limit
+    $max = 1024 * 1024 if !$max;
+
+    my $subject = defined($filename) ? "file '$filename'" : 'input';
+
+    my $br = 0;
+    my $input = '';
+    my $count;
+    while ($count = sysread($fh, $input, 8192, $br)) {
+        $br += $count;
+        die "$subject too long - aborting\n" if $br > $max;
+        if ($oneline && $input =~ m/^(.*)\n/) {
+            $input = $1;
+            last;
+        }
+    }
+    die "unable to read $subject - $!\n" if !defined($count);
+
+    return $input;
+}
+
+# creates a temporary file that does not shows up on the file system hierarchy.
+#
+# Uses O_TMPFILE if available, which makes it just an anon inode that never shows up in the FS.
+# If O_TMPFILE is not available, which unlikely nowadays (added in 3.11 kernel and all FS relevant
+# for us support it) back to open-create + immediate unlink while still holding the file  handle.
+#
+# TODO: to avoid FS dependent features we could (transparently) switch to memfd_create as backend
+sub tempfile($perm, %opts) {
+    # default permissions are stricter than with file_set_contents
+    $perm = 0600 if !defined($perm);
+
+    my $dir = $opts{dir};
+    if (!$dir) {
+        if (-d "/run/user/$<") {
+            $dir = "/run/user/$<";
+        } elsif ($< == 0) {
+            $dir = "/run";
+        } else {
+            $dir = "/tmp";
+        }
+    }
+    my $mode = $opts{mode} // O_RDWR;
+    $mode |= O_EXCL if !$opts{allow_links};
+
+    my $fh = IO::File->new($dir, $mode | O_TMPFILE, $perm);
+    if (!$fh && $! == EOPNOTSUPP) {
+        $dir = '/tmp' if !defined($opts{dir});
+        $dir .= "/.tmpfile.$$";
+        $fh = IO::File->new($dir, $mode | O_CREAT | O_EXCL, $perm);
+        unlink($dir) if $fh;
+    }
+    die "failed to create tempfile: $!\n" if !$fh;
+    return $fh;
+}
+
+# create an (ideally) anon file with the $data as content and return its FD-path and FH
+sub tempfile_contents($data, $perm, %opts) {
+    my $fh = tempfile($perm, %opts);
+    eval {
+        die "unable to write to tempfile: $!\n" if !print {$fh} $data;
+        die "unable to flush to tempfile: $!\n" if !defined($fh->flush());
+    };
+    if (my $err = $@) {
+        close $fh;
+        die $err;
+    }
+
+    return ("/proc/$$/fd/" . $fh->fileno, $fh);
+}
+
+sub dir_glob_regex($dir, $regex) {
+    my $dh = IO::Dir->new($dir);
+    return wantarray ? () : undef if !$dh;
+
+    while (defined(my $tmp = $dh->read)) {
+        if (my @res = $tmp =~ m/^($regex)$/) {
+            $dh->close;
+            return wantarray ? @res : $tmp;
+        }
+    }
+    $dh->close;
+
+    return wantarray ? () : undef;
+}
+
+sub dir_glob_foreach($dir, $regex, $func) {
+    my $dh = IO::Dir->new($dir);
+    if (defined $dh) {
+        while (defined(my $tmp = $dh->read)) {
+            if (my @res = $tmp =~ m/^($regex)$/) {
+                $func->(@res);
+            }
+        }
+    }
+}
+
+1;

src/Makefile
@@ -16,6 +16,7 @@ LIB_SOURCES = \
 	CpuSet.pm \
 	Daemon.pm \
 	Exception.pm \
+	File.pm \
 	Format.pm \
 	INotify.pm \
 	IPRoute2.pm \

src/PVE/Tools.pm
@@ -28,6 +28,7 @@ use Time::HiRes qw(usleep gettimeofday tv_interval alarm);
 use URI::Escape;
 use base 'Exporter';
 
+use PVE::File;
 use PVE::Syscall;
 use PVE::UPID;
 
@@ -113,9 +114,9 @@ use constant {
 };
 
 use constant {
-    O_PATH => 0x00200000,
-    O_CLOEXEC => 0x00080000,
-    O_TMPFILE => 0x00400000 | O_DIRECTORY,
+    O_PATH => PVE::File::O_PATH,
+    O_CLOEXEC => PVE::File::O_CLOEXEC,
+    O_TMPFILE => PVE::File::O_TMPFILE,
 };
 
 use constant {
@@ -273,120 +274,31 @@ sub lock_file {
     return lock_file_full($filename, $timeout, 0, $code, @param);
 }
 
+# file_ helper for backward compat, prefer PVE::File directly for new usage.
+# TODO: remove with PVE 10 / Debian Forky releases.
 sub file_set_contents {
     my ($filename, $data, $perm, $force_utf8) = @_;
-
-    $perm = 0644 if !defined($perm);
-
-    my $tmpname = "$filename.tmp.$$";
-
-    eval {
-        my ($fh, $tries) = (undef, 0);
-        while (!$fh && $tries++ < 3) {
-            $fh = IO::File->new($tmpname, O_WRONLY | O_CREAT | O_EXCL, $perm);
-            if (!$fh && $! == EEXIST) {
-                unlink($tmpname) or die "unable to delete old temp file: $!\n";
-            }
-        }
-        die "unable to open file '$tmpname' - $!\n" if !$fh;
-
-        if ($force_utf8) {
-            $data = encode("utf8", $data);
-        } else {
-            # Encode wide characters with print before passing them to syswrite
-            my $unencoded_data = $data;
-            # Preload PerlIO::scalar at compile time to prevent runtime loading issues when
-            # file_set_contents is called with PVE::LXC::Setup::protected_call. Normally,
-            # PerlIO::scalar is loaded implicitly during the execution of
-            # `open(my $data_fh, '>', \$data)`. However, this fails if it is executed within a
-            # chroot environment where the necessary PerlIO.pm module file is inaccessible.
-            # Preloading the module ensures it is available regardless of the execution context.
-            use PerlIO::scalar;
-            open(my $data_fh, '>', \$data) or die "failed to open in-memory variable - $!\n";
-            print $data_fh $unencoded_data;
-            close($data_fh);
-        }
-
-        my $offset = 0;
-        my $len = length($data);
-
-        while ($offset < $len) {
-            my $written_bytes = syswrite($fh, $data, $len - $offset, $offset)
-                or die "unable to write '$tmpname' - $!\n";
-            $offset += $written_bytes;
-        }
-
-        close $fh or die "closing file '$tmpname' failed - $!\n";
-    };
-    my $err = $@;
-
-    if ($err) {
-        unlink $tmpname;
-        die $err;
-    }
-
-    if (!rename($tmpname, $filename)) {
-        my $msg = "close (rename) atomic file '$filename' failed: $!\n";
-        unlink $tmpname;
-        die $msg;
-    }
+    return PVE::File::file_set_contents($filename, $data, $perm, $force_utf8);
 }
 
 sub file_get_contents {
     my ($filename, $max) = @_;
-
-    my $fh = IO::File->new($filename, "r")
-        || die "can't open '$filename' - $!\n";
-
-    my $content = safe_read_from($fh, $max, 0, $filename);
-
-    close $fh;
-
-    return $content;
+    return PVE::File::file_get_contents($filename, $max);
 }
 
 sub file_copy {
     my ($filename, $dst, $max, $perm) = @_;
-
-    file_set_contents($dst, file_get_contents($filename, $max), $perm);
+    PVE::File::file_copy($filename, $dst, $max, $perm);
 }
 
 sub file_read_firstline {
     my ($filename) = @_;
-
-    my $fh = IO::File->new($filename, "r");
-    if (!$fh) {
-        return undef if $! == POSIX::ENOENT;
-        die "file '$filename' exists but open for reading failed - $!\n";
-    }
-    my $res = <$fh>;
-    chomp $res if $res;
-    $fh->close;
-    return $res;
+    return PVE::File::file_read_first_line($filename);
 }
 
 sub safe_read_from {
     my ($fh, $max, $oneline, $filename) = @_;
-
-    # pmxcfs file size limit
-    $max = 1024 * 1024 if !$max;
-
-    my $subject = defined($filename) ? "file '$filename'" : 'input';
-
-    my $br = 0;
-    my $input = '';
-    my $count;
-    while ($count = sysread($fh, $input, 8192, $br)) {
-        $br += $count;
-        die "$subject too long - aborting\n" if $br > $max;
-        if ($oneline && $input =~ m/^(.*)\n/) {
-            $input = $1;
-            last;
-        }
-    }
-    die "unable to read $subject - $!\n" if !defined($count);
-
-    return $input;
+    return PVE::File::safe_read_from($fh, $max, $oneline, $filename);
 }
 
 # The $cmd parameter can be:
@@ -1296,32 +1208,12 @@ sub dump_journal {
 
 sub dir_glob_regex {
     my ($dir, $regex) = @_;
-
-    my $dh = IO::Dir->new($dir);
-    return wantarray ? () : undef if !$dh;
-
-    while (defined(my $tmp = $dh->read)) {
-        if (my @res = $tmp =~ m/^($regex)$/) {
-            $dh->close;
-            return wantarray ? @res : $tmp;
-        }
-    }
-    $dh->close;
-
-    return wantarray ? () : undef;
+    return PVE::File::dir_glob_regex($dir, $regex);
 }
 
 sub dir_glob_foreach {
     my ($dir, $regex, $func) = @_;
-
-    my $dh = IO::Dir->new($dir);
-    if (defined $dh) {
-        while (defined(my $tmp = $dh->read)) {
-            if (my @res = $tmp =~ m/^($regex)$/) {
-                &$func(@res);
-            }
-        }
-    }
+    PVE::File::dir_glob_foreach($dir, $regex, $func);
 }
 
 sub assert_if_modified {
@@ -1545,48 +1437,14 @@ sub sendmail {
 sub tempfile {
     my ($perm, %opts) = @_;
 
-    # default permissions are stricter than with file_set_contents
-    $perm = 0600 if !defined($perm);
-
-    my $dir = $opts{dir};
-    if (!$dir) {
-        if (-d "/run/user/$<") {
-            $dir = "/run/user/$<";
-        } elsif ($< == 0) {
-            $dir = "/run";
-        } else {
-            $dir = "/tmp";
-        }
-    }
-    my $mode = $opts{mode} // O_RDWR;
-    $mode |= O_EXCL if !$opts{allow_links};
-
-    my $fh = IO::File->new($dir, $mode | O_TMPFILE, $perm);
-    if (!$fh && $! == EOPNOTSUPP) {
-        $dir = '/tmp' if !defined($opts{dir});
-        $dir .= "/.tmpfile.$$";
-        $fh = IO::File->new($dir, $mode | O_CREAT | O_EXCL, $perm);
-        unlink($dir) if $fh;
-    }
-    die "failed to create tempfile: $!\n" if !$fh;
-    return $fh;
+    return PVE::File::tempfile($perm, %opts);
 }
 
 # create an (ideally) anon file with the $data as content and return its FD-path and FH
 sub tempfile_contents {
     my ($data, $perm, %opts) = @_;
 
-    my $fh = tempfile($perm, %opts);
-    eval {
-        die "unable to write to tempfile: $!\n" if !print {$fh} $data;
-        die "unable to flush to tempfile: $!\n" if !defined($fh->flush());
-    };
-    if (my $err = $@) {
-        close $fh;
-        die $err;
-    }
-
-    return ("/proc/$$/fd/" . $fh->fileno, $fh);
+    return PVE::File::tempfile_contents($data, $perm, %opts);
 }
 
 sub validate_ssh_public_keys {

commit c9a6ad626f06754a9a8f877075b7f178631d39be

file: add missing use statement
This fixes an "create storage failed: Undefined subroutine
&PVE::File::encode called" error when creating a PBS storage.

Fixes: 4b53440 ("create dedicated PVE::File module")
Signed-off-by: Friedrich Weber <f.weber@proxmox.com>
Reviewed-by: Gabriel Goller <g.goller@proxmox.com>
Link: https://lore.proxmox.com/20251105114117.40273-1-f.weber@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/File.pm
@@ -2,6 +2,7 @@ package PVE::File;
 
 use v5.36;
 
+use Encode qw(encode);
 use Fcntl qw(SEEK_SET SEEK_END);
 use File::Basename qw(dirname);
 use File::Path qw(make_path);