proxmox/pve-common

commit 057f62f73048bc1e73e45e9edf6e197f84de630a

fix #7118: fix bridge port detection when plugging netdev with vlan
When tagging a network device with a VLAN tag, tap_plug checks if the
bridge is vlan-aware and, if it isn't, creates a VLAN subinterface and
a respective bridge for that VLAN for the physical interfaces that are
enslaved on the bridge. The detection of physical interfaces relied on
a regex that only allowed certain prefixes. Since the introduction of
network-interface-pinning, the rules for network interface naming have
been changed, and physical network interfaces are not restricted to
certain prefixes anymore. Therefore, use the newly provided helper
from IPRoute2 that uses `ip link` to obtain the physical bridge ports,
instead of a regex.

Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20251210184236.448600-2-s.hanreich@proxmox.com

src/PVE/IPRoute2.pm
@@ -39,6 +39,19 @@ sub ip_link_is_bridge_member($ip_link) {
         && $ip_link->{linkinfo}->{info_slave_kind} eq "bridge";
 }
 
+sub get_physical_bridge_ports($bridge, $ip_links = undef) {
+    $ip_links = ip_link_details() if !defined($ip_links);
+
+    if (!ip_link_is_bridge($ip_links->{$bridge})) {
+        warn "passed link that isn't a bridge to get_physical_bridge_ports";
+        return ();
+    }
+
+    return grep {
+        ip_link_is_physical($ip_links->{$_}) && $ip_links->{$_}->{master} eq $bridge
+    } keys $ip_links->%*;
+}
+
 sub altname_mapping($ip_links) {
     $ip_links = ip_link_details() if !defined($ip_links);
 

src/PVE/Network.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use PVE::INotify;
+use PVE::IPRoute2;
 use PVE::ProcFSTools;
 use PVE::Tools qw(run_command lock_file);
 
@@ -684,16 +685,7 @@ sub activate_bridge_vlan {
 
     my $bridgevlan = "${bridge}v$tag";
 
-    my @ifaces = ();
-    my $dir = "/sys/class/net/$bridge/brif";
-    PVE::Tools::dir_glob_foreach(
-        $dir,
-        '(((eth|bond)\d+|en[^.]+)(\.\d+)?)',
-        sub {
-            push @ifaces, $_[0];
-        },
-    );
-
+    my @ifaces = PVE::IPRoute2::get_physical_bridge_ports($bridge);
     die "no physical interface on bridge '$bridge'\n" if scalar(@ifaces) == 0;
 
     lock_network(sub {

commit 8516622423e11789ef2801bd44f58803544eafab

fix #7153: iproute2: consider bonds as physical bridge ports
The new check did not consider bonds, since ip_link_is_physical only
returns true for physical interfaces - but not bonds. Add a new
function to check for bonds explicitly and use it when checking for
bridge ports in activate_bridge_vlan.

This bug caused VMs with network devices with VLAN tags, on
non-vlan-aware bridges, to not get started / migrated.

Fixes: 057f62f7
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Reviewed-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Tested-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20251216160513.360391-1-s.hanreich@proxmox.com

src/PVE/IPRoute2.pm
@@ -32,6 +32,14 @@ sub ip_link_is_physical($ip_link) {
         && (!defined($ip_link->{linkinfo}) || !defined($ip_link->{linkinfo}->{info_kind}));
 }
 
+sub ip_link_is_bond($ip_link) {
+    return
+        $ip_link->{link_type} eq 'ether'
+        && defined($ip_link->{linkinfo})
+        && defined($ip_link->{linkinfo}->{info_kind})
+        && $ip_link->{linkinfo}->{info_kind} eq 'bond';
+}
+
 sub ip_link_is_bridge($ip_link) {
     return
         defined($ip_link->{linkinfo})
@@ -67,7 +75,8 @@ sub get_physical_bridge_ports($bridge, $ip_links = undef) {
     }
 
     return grep {
-        ip_link_is_physical($ip_links->{$_}) && $ip_links->{$_}->{master} eq $bridge
+        (ip_link_is_physical($ip_links->{$_}) || ip_link_is_bond($ip_links->{$_}))
+            && defined($ip_links->{$_}->{master}) && $ip_links->{$_}->{master} eq $bridge
     } keys $ip_links->%*;
 }
 